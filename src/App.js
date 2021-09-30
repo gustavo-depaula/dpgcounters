1fc10a8bb0280cf7651db876acef481dc552e1b9
Gustavo de Paula <gustavodepaula@disroot.org>
2021-09-30 00:36:50 -0300

    basic counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 function Counter() {
-  return <div></div>;
+  return (
+    <div className="flex w-full justify-between max-w-sm mx-auto text-xl">
+      <span>Marbles</span>
+
+      <div className="text-center">
+        <button>-</button>
+        <span className="mx-4">0</span>
+        <button>+</button>
+      </div>
+    </div>
+  );
 }
 
 function App() {
   return (
-    <div className="App">
-      <h1>dpgcounters</h1>
+    <div className="App container mx-auto">
+      <h1 className="text-4xl mt-8 mb-32">dpgcounters</h1>
       <Counter></Counter>
     </div>
   );
