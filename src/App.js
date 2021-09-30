bee70c78dae761d6ef092e773c50b5fa6915c0eb
Gustavo de Paula <gustavodepaula@disroot.org>
2021-09-30 00:50:14 -0300

    basic counter inc/dec locally

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import { useState } from "react";
 import "./App.css";
 
-function Counter() {
+function Counter({ counter, updateCounter }) {
+  const makeOperation = (type) => () =>
+    updateCounter({ counterId: counter.id, type });
+
   return (
     <div className="flex w-full justify-between max-w-sm mx-auto text-xl">
       <span>Marbles</span>
 
       <div className="text-center">
-        <button>-</button>
-        <span className="mx-4">0</span>
-        <button>+</button>
+        <button onClick={makeOperation("dec")}>-</button>
+        <span className="mx-4">{counter.value}</span>
+        <button onClick={makeOperation("inc")}>+</button>
       </div>
     </div>
   );
 }
 
+function useCounterSet() {
+  const [counterSet, setCounterSet] = useState({
+    id: "",
+    counters: [
+      {
+        id: "3434",
+        value: 0,
+        unsignedOperationsAcks: [],
+      },
+    ],
+  });
+
+  const [mutations, setMutations] = useState([]);
+
+  const updateCounter = ({ type, counterId }) => {
+    setMutations((ms) => [...ms, { type, counterId, ack: "asdf" }]);
+  };
+
+  const countersById = {};
+  counterSet.counters.forEach((counter) => {
+    countersById[counter.id] = { ...counter };
+  });
+
+  mutations.forEach(({ type, counterId }) => {
+    if (type === "inc") {
+      countersById[counterId].value++;
+    }
+
+    if (type === "dec") {
+      countersById[counterId].value--;
+    }
+  });
+
+  const counters = Object.values(countersById);
+  console.log({ mutations, countersById });
+
+  return {
+    counters,
+    updateCounter,
+  };
+}
+
 function App() {
+  const { counters, updateCounter } = useCounterSet();
+
   return (
     <div className="App container mx-auto">
       <h1 className="text-4xl mt-8 mb-32">dpgcounters</h1>
-      <Counter></Counter>
+      {counters.map((counter) => (
+        <Counter
+          key={counter.id}
+          counter={counter}
+          updateCounter={updateCounter}
+        ></Counter>
+      ))}
     </div>
   );
 }
