2741fcaaebc91322938370cee289c6a651dcd12c
Gustavo de Paula <gustavodepaula@disroot.org>
2021-09-30 00:10:12 -0300

    use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Comic Sans MS"],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
